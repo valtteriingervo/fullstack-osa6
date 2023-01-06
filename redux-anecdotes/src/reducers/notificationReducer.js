893135420c5efeb4d8519ea6d8dc9f8c862af9ea
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2023-01-06 12:09:16 +0200

    6.21

--- redux-anecdotes/src/reducers/notificationReducer.js
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+let lastTimerID = null
 const initialState = ''
 
 const notificationSlice = createSlice({
@@ -15,14 +16,28 @@ const notificationSlice = createSlice({
   }
 })
 
+
 export const { changeNotif, changeNotifForVote, clearNotif } = notificationSlice.actions
 
 export const setNotification = (notifText, timeInSec) => {
   return async dispatch => {
     dispatch(changeNotif(notifText))
-    setTimeout(() => {
+
+    // Clear the previous timer (if there has been one)
+    if (lastTimerID) {
+      clearTimeout(lastTimerID)
+    }
+    // Start a new timer
+    const timeoutID = setTimeout(() => {
       dispatch(clearNotif())
     }, timeInSec * 1000)
+
+    console.log('lastTimerID', lastTimerID)
+    console.log('timeoutID', timeoutID)
+
+    // Set the global variable holding the previous timer ID
+    lastTimerID = timeoutID
+
   }
 }
 
